eda86318fa413675afb9c3a30792b676b239b8ce
adnikolaeva <81370742+adnikolaeva@users.noreply.github.com>
2021-04-18 19:59:16 +0500

    1

---
+++ Statica/Statica.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "DLL2.h"
+#include "DLL2.cpp"
+
+
+extern "C" __declspec(dllimport) int Group();
+extern "C" __declspec(dllimport) void Name(char* name1, int n);
+
+int main()
+{
+	std::cout << "Group\n" << Group() << "\n";
+
+	char vi[20];
+	Name(vi, 20);
+	std::cout << "Name\n" << vi << "\n";
+	system("pause");
+	return 0;
+}
