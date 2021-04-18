d197d2f01819e724ee5b16d93ac19e79006e77cb
adnikolaeva <81370742+adnikolaeva@users.noreply.github.com>
2021-04-18 20:01:20 +0500

    1

---
+++ Statica/Dll2.cpp
@@ -0,0 +1,17 @@
+
+#include "Dll2.h"
+#include "string.h"
+
+int Group()
+{
+	int Group = 561220;
+		return Group;
+		
+}
+
+void Name(char* name1, int n)
+{
+strcpy_s(name1, n, "Anna Nikolaeva");
+}
+
+
---
+++ Statica/Dll2.h
@@ -0,0 +1,5 @@
+#pragma once
+extern "C" __declspec (dllexport) int Group();
+extern "C" __declspec (dllexport) void Name(char* name1, int n);
+
+
